a469fc63b882856144374f9e7327137f0d227ccc
unknown <connordupuis760@gmail.com>
2021-12-04 04:19:11 -0500

    Send data using websocket over WiFi

--- platform/esp32/src/main.cpp
+++ platform/esp32/src/main.cpp
@@ -2,6 +2,8 @@
 #include <Wire.h>
 #include <Arduino_LSM6DS3.h>
 #include <string>
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
 #include "filesystems.h"
 #include "NeuralNetwork.h"
 #include "model_data.h"
@@ -10,8 +12,22 @@
 
 void spiffManualWriteDefualtModel();
 void spiffWriteNewModel(char[], int, std::string);
+void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len);
 
+/* 
+Credits to Nuno Santos's blog for websocket help. See article below:
+ https://techtutorialsx.com/2018/09/13/esp32-arduino-web-server-receiving-data-from-javascript-websocket-client/ for websocket help 
+ */
+
+// WiFi and Websocket variables
+const char* ssid = "Actiontec-7830-2.4G";
+const char* password =  "42389e65";
+AsyncWebServer server(80);
+AsyncWebSocket ws("/websocket");
+
+// Neural network object
 NeuralNetwork *nn;
+
 int mode = 0;
 int modePin = 4;
 
@@ -34,15 +50,35 @@ void setup() {
 	// Initialize IMU
 	if (!IMU.begin()) {
 		Serial.println("Failed to initialize IMU!");
-		while (1);
+		return;
 	}
+
+	//SPIFFS.remove("/userModel.txt");
+
+	listDir(SPIFFS, "/", 1);
+
+	// Connect to WiFi and start server
+	Serial.println("Connecting to WiFi");
+	WiFi.begin(ssid, password);
+	if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+		Serial.println("WiFi Failed!");
+		return;
+	}
+	Serial.println(WiFi.localIP());
+
+	ws.onEvent(onWsEvent);
+	server.addHandler(&ws);
+	server.on("/html", HTTP_GET, [](AsyncWebServerRequest *request){
+		request->send(SPIFFS, "/websocket.html", "text/html");
+	});
+	server.begin();
 
 	// Manual switch for swicthing modes
 	pinMode(4, INPUT);
 
-	//Need to build with this uncommented once, then it is saved in memory for future build.
+	// Need to build with this uncommented once, then it is saved in memory for future build.
  	//spiffWriteNewModel(converted_model_tflite, converted_model_tflite_len, "/defaultModel.txt");
-
+	 
 	// New neural network object
 	nn = new NeuralNetwork();
 }
@@ -108,20 +144,25 @@ void loop() {
 		delay(10);
 	}
 	else {
-		/*Receive model from external device and assign in to values*/
-		if(Serial.available()){
-			String command = Serial.readString();
-			unsigned int new_model_tflite_len = std::atoi(command.c_str());
-			Serial.println((String) "The new  model has  a length of " + command);
+		// Receive model through websocket and save it 
+		
 
-			char new_model_tflite[new_model_tflite_len];
-			Serial.readBytes(new_model_tflite, new_model_tflite_len);
-			Serial.println((String) "The new  model has  been read ");
+		// Receive model from through serial external device and assign in to values
+		// if(Serial.available()){
+		// 	String command = Serial.readString();
+		// 	unsigned int new_model_tflite_len = std::atoi(command.c_str());
+		// 	Serial.println((String) "The new  model has  a length of " + command);
 
-			/*Writes the new model to memory (userModel)*/
-			spiffWriteNewModel(new_model_tflite, new_model_tflite_len, "/userModel.txt");
-			Serial.println((String)"Done writing new model to memory");
-		}
+		// 	Serial.flush();
+
+		// 	char new_model_tflite[new_model_tflite_len];
+		// 	Serial.readBytes(new_model_tflite, new_model_tflite_len);
+		// 	Serial.println((String) "The new  model has  been read ");
+
+		// 	//Writes the new model to memory (userModel)
+		// 	spiffWriteNewModel(new_model_tflite, new_model_tflite_len, "/userModel.txt");
+		// 	Serial.println((String)"Done writing new model to memory");
+		// }
 	}
 }
 
@@ -145,8 +186,8 @@ void spiffWriteNewModel(char  newModel[], int newModelLen, std::string path){
 }
 
 /*
-Don't need to write the default model to memory because it already exists (model.cpp). This fucntion was used to test the SPIFF filesystem
-and see if writing and reading a model works as intended. Thas was ncessary because there is no current functionality to get a user model
+Don't need to write the default model to memory because it already exists (model.cpp). This function was used to test the SPIFF filesystem
+and see if writing and reading a model works as intended. This was necessary because there was no functionality to get a user model
 to the device to write to memory and read from.
 */
 // Writes the defualt model to the ESP32. If a model already exists it will just return.
@@ -167,5 +208,73 @@ void spiffManualWriteDefualtModel(){
 	}
 	else{
 		Serial.println("Default model already exists, skipping step");
+	}
+}
+
+// Handles websocket events
+void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
+	if(type == WS_EVT_CONNECT){
+		Serial.println("Websocket client connection received");
+	}
+	else if(type == WS_EVT_DISCONNECT){
+		Serial.println("Client disconnected");
+		Serial.println("-----------------------");
+	} 
+	else if(type == WS_EVT_DATA){
+		//data packet
+		AwsFrameInfo * info = (AwsFrameInfo*)arg;
+
+		char new_model_tflite[info->len];
+		int new_model_tflite_len = info->len;
+
+		if(info->final && info->index == 0 && info->len == len){
+			//the whole message is in a single frame and we got all of it's data
+			printf("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT)?"text":"binary", info->len);
+			if(info->opcode == WS_TEXT){
+				data[len] = 0;
+				printf("%s\n", (char*)data);
+			} else {
+				for(size_t i=0; i < info->len; i++){
+					printf("%02x ", data[i]);
+				}
+				printf("\n");
+			}
+			if(info->opcode == WS_TEXT)
+				client->text("I got your text message");
+			else
+				client->binary("I got your binary message");
+		}
+		else {
+			//message is comprised of multiple frames or the frame is split into multiple packets
+			if(info->index == 0){
+				if(info->num == 0)
+					printf("ws[%s][%u] %s-message start\n", server->url(), client->id(), (info->message_opcode == WS_TEXT)?"text":"binary");
+				printf("ws[%s][%u] frame[%u] start[%llu]\n", server->url(), client->id(), info->num, info->len);
+			}
+
+			printf("ws[%s][%u] frame[%u] %s[%llu - %llu]: ", server->url(), client->id(), info->num, (info->message_opcode == WS_TEXT)?"text":"binary", info->index, info->index + len);
+			if(info->message_opcode == WS_TEXT){
+				data[len] = 0;
+				printf("%s\n", (char*)data);
+			} else {
+				for(size_t i=0; i < len; i++){
+					printf("%02x ", data[i]);
+					new_model_tflite[info->index + i] = (char)data[i];
+				}
+				printf("\n");
+			}
+
+			if((info->index + len) == info->len){
+				printf("ws[%s][%u] frame[%u] end[%llu]\n", server->url(), client->id(), info->num, info->len);
+				if(info->final){
+					spiffWriteNewModel(new_model_tflite, new_model_tflite_len, "/userModel.txt");
+					printf("ws[%s][%u] %s-message end\n", server->url(), client->id(), (info->message_opcode == WS_TEXT)?"text":"binary");
+					if(info->message_opcode == WS_TEXT)
+						client->text("I got your text message");
+					else
+						client->binary("I got your binary message");
+				}
+			}
+		}
 	}
 }
